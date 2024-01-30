6b37cb084de27978b0b813bb0a274b1da9f5ceba
Jasonlya <1210616455@qq.com>
2024-01-30 22:25:17 +0800

    数据库断言

--- testcases/login/test_demo.py
+++ testcases/login/test_demo.py
@@ -24,3 +24,11 @@ def test_case_1():
 def test_case_2():
     # 测试步骤和断言
     pass
+
+'''
+    with allure.step("数据库断言检查"):
+        sql = Sql_base('host', 'port', 'user', 'password', 'database')
+        # sql.execute("sql语句").fetchall()  # 获取全部SQL结果
+        sql_data = sql.execute("sql语句").fetchone()  # 获取一条SQL结果
+        assert sql_data == "要断言的数据", "断言失败信息"
+'''
